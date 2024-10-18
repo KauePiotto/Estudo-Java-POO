b914863f71c57bf429c4a460cba4bfb8a2e19adb
KauePiotto <kauepiotto@gmail.com>
2024-10-18 10:17:39 -0300

    aulas-poo

--- projetopoo/src/main/java/br/senac/sp/projetopoo/view/FrameMarca.java
+++ projetopoo/src/main/java/br/senac/sp/projetopoo/view/FrameMarca.java
@@ -1,17 +1,19 @@
 package br.senac.sp.projetopoo.view;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import br.senac.sp.projetopoo.modelo.Marca;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextPane;
 import java.awt.Color;
+import javax.swing.JTable;
 
 public class FrameMarca extends JFrame {
 
@@ -19,6 +21,8 @@ public class FrameMarca extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtId;
 	private JTextField txtNome;
+	private JTable tblMarca;
+	private Marca marca;
 
 	/**
 	 * Launch the application.
@@ -67,11 +71,8 @@ public class FrameMarca extends JFrame {
 		contentPane.add(txtNome);
 		txtNome.setColumns(10);
 		
-		JButton btnSalvar = new JButton("Salvar");
-		btnSalvar.setBounds(10, 97, 131, 34);
-		contentPane.add(btnSalvar);
-		
 		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.setMnemonic('e');
 		btnExcluir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -80,17 +81,30 @@ public class FrameMarca extends JFrame {
 		contentPane.add(btnExcluir);
 		
 		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setMnemonic('l');
 		btnLimpar.setBounds(301, 97, 123, 34);
 		contentPane.add(btnLimpar);
-		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(10, 142, 414, 108);
-		contentPane.add(textPane);
 		
 		JLabel lblLogo = new JLabel("");
 		lblLogo.setBackground(new Color(192, 192, 192));
 		lblLogo.setOpaque(true);
 		lblLogo.setBounds(370, 11, 54, 61);
 		contentPane.add(lblLogo);
+		
+		tblMarca = new JTable();
+		tblMarca.setBounds(10, 142, 414, 108);
+		contentPane.add(tblMarca);
+		
+		JButton btnSalvar = new JButton("Salvar");
+		btnSalvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtNome.getText().trim().isEmpty()) {
+					JOptionPane.showMessageDialog(null,"Informe o nome","Aviso",JOptionPane.INFORMATION_MESSAGE);
+					txtNome.requestFocus();
+				}
+			}
+		});
+		btnSalvar.setBounds(10, 97, 131, 34);
+		contentPane.add(btnSalvar);
 	}
 }
