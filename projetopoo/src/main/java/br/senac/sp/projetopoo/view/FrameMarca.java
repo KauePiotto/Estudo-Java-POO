61cc89a5eac911124688c7213f281af440ebdda5
KauePiotto <kauepiotto@gmail.com>
2024-10-18 09:29:28 -0300

    aulas-poo

--- projetopoo/src/main/java/br/senac/sp/projetopoo/view/FrameMarca.java
+++ projetopoo/src/main/java/br/senac/sp/projetopoo/view/FrameMarca.java
@@ -5,11 +5,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextPane;
+import java.awt.Color;
 
 public class FrameMarca extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtId;
+	private JTextField txtNome;
 
 	/**
 	 * Launch the application.
@@ -37,6 +46,51 @@ public class FrameMarca extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNome = new JLabel("Nome");
+		lblNome.setBounds(10, 47, 75, 25);
+		contentPane.add(lblNome);
+		
+		JLabel lblId = new JLabel("ID:");
+		lblId.setBounds(10, 11, 75, 25);
+		contentPane.add(lblId);
+		
+		txtId = new JTextField();
+		txtId.setEditable(false);
+		txtId.setBounds(45, 13, 75, 23);
+		contentPane.add(txtId);
+		txtId.setColumns(10);
+		
+		txtNome = new JTextField();
+		txtNome.setBounds(45, 49, 315, 23);
+		contentPane.add(txtNome);
+		txtNome.setColumns(10);
+		
+		JButton btnSalvar = new JButton("Salvar");
+		btnSalvar.setBounds(10, 97, 131, 34);
+		contentPane.add(btnSalvar);
+		
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnExcluir.setBounds(151, 97, 140, 34);
+		contentPane.add(btnExcluir);
+		
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setBounds(301, 97, 123, 34);
+		contentPane.add(btnLimpar);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(10, 142, 414, 108);
+		contentPane.add(textPane);
+		
+		JLabel lblLogo = new JLabel("");
+		lblLogo.setBackground(new Color(192, 192, 192));
+		lblLogo.setOpaque(true);
+		lblLogo.setBounds(370, 11, 54, 61);
+		contentPane.add(lblLogo);
 	}
-
 }
